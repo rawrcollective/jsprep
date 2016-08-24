b286e2a5a406a9b166d9af0f914ea93f8556ef75
ruparocks <RSharma@greendotcorp.com>
2016-08-23 22:24:31 -0700

    Add skeleton for Week One Section 1

---
+++ Week 2/01-JS-and-the-DOM/2.1.4-js-exercises/exercise.js
@@ -0,0 +1,22 @@
+// 2.1.4 Exercise: ------------------------------------ /
+// Exercise 1:
+// 1. Declare variables a, b and c. Assign a number to each.
+// 2. Declare variable d and assign it the value of a, b, and c added together.
+// 3. Re-assign the value of b to a. Then change the value of b. What is the value of a?
+
+//--------------- TYPE YOUR CODE HERE ------------------- /
+
+// Exercise 2:
+// 1. Create variables "hour" and "minute" to reflect the current time.
+// 2. Write an expression to calculate how many seconds until midnight.
+// 3. Change "minute" to be accurate again as time has probably passed.
+// 4. Run the same calculation again (press the up key in the console to scroll back to it).
+
+//--------------- TYPE YOUR CODE HERE ------------------- /
+
+// Exercise 3:
+// 1. Declare at least 10 variables such as 'name', 'age', 'birthday', 'hometown', 'occupation', 'favoriteFood', 'bestFriend', etc, and set their values equal to strings containing the appropriate information about yourself.
+// 2. console.log a paragraph about yourself using the appropriate variables to insert the relevant information in the appropriate places.
+// 3. Create a new HTML document and re-write the variables and the console.log in a <script> tag so that you'll have it for later.
+
+//--------------- TYPE YOUR CODE HERE ------------------- /
