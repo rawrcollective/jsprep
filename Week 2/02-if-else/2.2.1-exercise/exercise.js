67e971a183121466bba045a24b5231f1574bfbec
ruparocks <RSharma@greendotcorp.com>
2016-08-23 22:31:44 -0700

    Skeleton for Week 2 Part 2

---
+++ Week 2/02-if-else/2.2.1-exercise/exercise.js
@@ -0,0 +1,8 @@
+// 2.2.1 Exercises:
+// 1. Write a if statement that always runs its code block
+// 2. Write a if statement that never runs its code block
+// 3. Write a statement that logs a message of encouragement if a visitor, when prompted, responds that they are sad.
+// 4. Write a statement that alerts a warning if a visitor, when prompted, does not guess that the secret number is 3.
+// 5. Prompt the visitor to enter a password when they visit your site. If it is correct, welcome them. Otherwise, direct them elsewhere using this line of code:
+
+//--------------- TYPE YOUR CODE HERE ------------------- /
